beff9bc75c38bbc3be36e8a9ee4e877ea1c77d32
Marco Berardini <mberardini@evolution-man.com>
2017-03-21 14:14:33 -0700

    [client] created proposal class and proposal new component.

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core'
+import { Proposal } from './proposal'
 
 
 
 @Component({
   selector: 'app-proposal-list',
   template: `
-    <div>Proposal list </div>
+    <h1>Proposal list </h1>
+    <div *ngFor='let prop of proposals'> {{prop.id}} - {{prop.customer}} - {{prop.portfolio_url}}</div>
   
   `
 })
 
 export class ProposalListComponent {
-
-
+  proposalOne: Proposal = new Proposal(1, 'Fake Company', 'http://fake.com','Ruby',150, 220, 15, 'marcoberardini@gmail.com')
+  proposalTwo: Proposal = new Proposal(2, '2 Fake Company','http://2fake.com, 'Javascript', 110, 220, 10, 'marcoberardini@gmail.com')
+  proposalThree: Proposal = new Proposal(3, '3 Fake Company', 'http://3fake.com', 'Python' ,40, 220, 1, 'marcoberardini@gmail.com')
   
+
+  proposals: Proposal[] = [ this.proposalOne, this.proposalTwo, this.proposalThree ]
+
 }
---
+++ src/app/proposal/proposal-new-component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { Proposal } from './proposal'
+
+@Component({
+  selector: "app-proposal-new",
+  template: `
+  
+  `
+})
+
+export class ProposalNewComponent {
+  
+}
