0ea1623d15b7774ac8a9aa7dc1b9d0fd3f531764
Marco Berardini <mberardini@evolution-man.com>
2017-03-21 15:41:17 -0700

    [client] Started building out proposal form.

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -5,6 +5,21 @@ import { Proposal } from './proposal'
   selector: "app-proposal-new",
   template: `
   <h1>Create new Proposal</h1>
+  <div>
+    <form #proposalForm="ngForm">
+      <label for="customer">Customer Name</label>
+      <input  type="text"
+              id="customer"
+              placeholder="ABC Company"
+              name="customer"
+              #customer
+              [(ngModel)]="proposal.customer"
+      >
+    
+    </form>
+  
+    <div>{{ proposal.customer }}</div>
+  </div>
   `
 })
 
